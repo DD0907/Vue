93e67c4c0b72a1fde504928e63e2b78e535f6f29
DD0907 <669135386@qq.com>
2018-05-06 19:47:15 +0800

    Vue项目

---
+++ ping/ping/src/components/view/PageDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h2>{{goodsId}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{
+        goodsId: ""
+      }
+  },
+  mounted(){
+      this.getParams();
+  },
+  methods:{
+      getParams() {
+      // 取到路由带过来的参数
+      var routerParams = this.$route.params.goodsId;
+      // 将数据放在当前组件的数据内
+      this.goodsId = routerParams;
+    //   this.$toast(routerParams);
+    },
+  }
+}
+</script>
+
---
+++ ping/ping/src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/components/view/index'
+import indexs from '@/components/view/indexs'
+import search from '@/components/view/search'
+import test from "@/components/view/test"
+import { resolve } from 'path';
+import selecteddata from '@/components/view/selecteddata'
+import fruitdata from '@/components/view/fruitdata'
+import PageDetails from '@/components/view/PageDetails'
+Vue.use(Router)
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/ping/index',
+      name: 'index',
+      component:resolve => require(['@/components/view/index'],resolve),
+      // component: index,
+      meta:{keepAlive: true}
+    },
+    {
+      path: '/ping/search',
+      name: 'search',
+      component: search
+    },
+    {
+      path: '/ping/indexs',
+      name: 'indexs',
+      component: indexs
+    },
+    {
+      path: '/ping/test',
+      name: 'test',
+      component: test
+    },
+    {
+      path: '/ping/selecteddata',
+      name: 'selecteddata',
+      component: selecteddata
+    },
+    {
+      path: '/ping/fruitdata',
+      name: 'fruitdata',
+      component: fruitdata
+    },
+    {
+      path: '/ping/PageDetails',
+      name: 'PageDetails',
+      component: PageDetails
+    }
+  ]
+})
